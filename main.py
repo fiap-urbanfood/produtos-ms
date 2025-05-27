397d4ba0d9fe3fa56c8b500b8bfe694619a00606
Giovane Nunes <giovane.nunes@silro.com.br>
2025-05-26 23:51:07 -0300

    update port service

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000,
+    uvicorn.run("main:app", host="0.0.0.0", port=8003,
                 log_level='info', reload=True)
