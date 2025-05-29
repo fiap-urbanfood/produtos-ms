c0c5d3d2345f81fd04f358a4d9f5e48dff269540
Giovane Nunes <giovane.nunes@silro.com.br>
2025-05-28 21:46:12 -0300

    update config

--- core/configs.py
+++ core/configs.py
@@ -10,22 +10,19 @@ class Settings(BaseSettings):
 
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = 'UrbanFood - Microserviço de Produtos'
-    # filepath: /home/galaxybook2/Documentos/repositorio/famp/secao04/core/configs.py
-    # DB_URL: str = "postgresql+asyncpg://teste_user:ollpemjnlwsg@db-57878.dc-sp-1.absamcloud.com:11087/teste"
     DB_URL: str = (
-        "postgresql+asyncpg://postgres.tascqincqdsjvmdjdwnh:123456@aws-0-us-east-2.pooler.supabase.com:5432/postgres"
+        "mysql+aiomysql://urbanfood:Urbanf00dFiap@rds-mysql.c8gkm8vsq6yc.us-east-1.rds.amazonaws.com:3306/urbanfood"
     )
-
-    # URLs dos microserviços
-    #LOGIN_SERVICE_URL: str = 'http://localhost:8001'
-    LOGIN_SERVICE_URL: str = 'http://a290354dd0cfd40cbb428316c51cd3ea-2025820054.us-east-1.elb.amazonaws.com:8001'  # URL do microserviço de login
 
     DBBaseModel: ClassVar = (
         declarative_base()
     )  # Adicionando ClassVar para evitar o erro
 
+    # URL do microserviço de login
+    #LOGIN_SERVICE_URL: str = 'http://localhost:8001'
+    LOGIN_SERVICE_URL: str = 'http://a290354dd0cfd40cbb428316c51cd3ea-2025820054.us-east-1.elb.amazonaws.com:8001'
+
     class Config:
         case_sensitive = True
-
 
 settings = Settings()
